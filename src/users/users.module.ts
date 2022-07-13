a1777007768eec95f659b7b05b23238acb0c8159
Lucas Almeida <lucasaclima03@gmail.com>
2022-07-13 14:50:39 -0300

    feat: add create user dto and user service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { UsersService } from './users.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserRepository])]
+    imports: [TypeOrmModule.forFeature([UserRepository])],
+    providers: [UsersService]
 })
 export class UsersModule {}
