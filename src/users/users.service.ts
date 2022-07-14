99c9cbf02399057a411851bfec2d97bf38124435
Lucas Almeida <lucasaclima03@gmail.com>
2022-07-14 10:47:02 -0300

    feat: add createAdminUser method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './create-user.dto';
+import { UserRole } from './user-roles.enum';
+import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -8,4 +11,12 @@ export class UsersService {
         @InjectRepository(UserRepository)
         private userRepository: UserRepository
     ){}
+    async createAdminUser(createUserDto: CreateUserDto, ): Promise<User>{
+        if(createUserDto.password != createUserDto.passwordConfirmation){
+            throw new UnprocessableEntityException('As senhas não conferem')
+        } else {
+            return this.userRepository.createUser(createUserDto, UserRole.ADMIN)
+        }
+
+    }
 }
